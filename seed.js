cb1299dff139c3942f02e907df3947b1c3595126
Jonathan Johnson <jraf7@icloud.com>
2019-12-02 22:01:45 -0500

    intial inventory

---
+++ seed.js
@@ -0,0 +1,83 @@
+let seed = [
+
+    {
+        product_name: "Red Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Orange Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Yellow Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Green Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Blue Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Indigo Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Violet Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Black Crayon",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Paper",
+        department_name: "Arts & Crafts",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Chair",
+        department_name: "Furniture",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Table",
+        department_name: "Furniture",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Vase",
+        department_name: "Decor",
+        price: 1,
+        stock_quantity: 100
+    },
+    {
+        product_name: "Lava Lamp",
+        department_name: "Decor",
+        price: 1,
+        stock_quantity: 100
+    }
+]
+
+module.exports = seed;
